19b9c94398c58442ce06bb85234d1149206eb83b
eduyoshiaki <eduardokaneko4@gmail.com>
2024-04-20 09:22:15 -0300

    exercício 2

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ let b = (8)
 
 let soma = (a + b)
 
-console.log(soma)
+console.log("Resultado da soma: ", soma);
+
+function verificaNumero(numero) {
+    if (numero % 2 === 0) {
+        console.log(numero + " é par.");
+    } else {
+        console.log(numero + " é impar.");
+    }
+}
+
+verificaNumero(5);
+verificaNumero(2);
+verificaNumero(14);
+verificaNumero(9);
+verificaNumero(6);
